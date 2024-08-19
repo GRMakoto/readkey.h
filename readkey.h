dc30f454a0df10c121bf5f869fe8a58d7b2a91c0
Makoto <no@email.com>
2024-08-20 01:14:19 +0300

    windows implementation

--- readkey.h
+++ readkey.h
@@ -1,24 +1,83 @@
+#ifndef READKEY_H
+#define READKEY_H
+
+#ifdef _WIN32
+#include <windows.h>
+#else
 #include <unistd.h>
 #include <termios.h>
+#endif
+
+#if defined __cplusplus
+extern "C" {
+#endif
 
 static char read_key();
 
+#ifdef _WIN32
+char read_key() {
+    HANDLE hStdin ;
+    INPUT_RECORD ir;
+    DWORD cNumRead;
+    if ((hStdin = GetStdHandle(STD_INPUT_HANDLE)) == INVALID_HANDLE_VALUE) {
+        return -1;
+    }
+
+    int max_iter = 10000;
+    do {
+        printf("Max iter: %d", max_iter);
+        if(!ReadConsoleInput(
+            hStdin,
+            &ir,
+            1,
+            &cNumRead)) {
+            return -1;
+        }
+
+        if(ir.EventType == KEY_EVENT) {
+            if (ir.Event.KeyEvent.bKeyDown && ir.Event.KeyEvent.wVirtualKeyCode != VK_SHIFT) {
+                return ir.Event.KeyEvent.uChar.AsciiChar;
+            }
+        }
+    } while(max_iter-- > 0);
+
+    return -1;
+}
+
+
+
+#else
 static char read_key() {
     static struct termios oldt, newt;
-    tcgetattr(0, &oldt);
+    if(tcgetattr(0, &oldt) == -1) {
+        return -1;
+    }
 
     newt = oldt;
     newt.c_lflag &= ~(ECHO | ICANON);
     newt.c_cc[VMIN] = 1;
     newt.c_cc[VTIME] = 0;
 
-    tcsetattr(0, TCSANOW, &newt);
-
-    char c;
-    if(read(0, &c, 1) == -1){
+    if(tcsetattr(0, TCSANOW, &newt) == -1) {
         return -1;
     }
 
-    tcsetattr(1, TCSANOW, &oldt);
+    char c;
+    if(read(0, &c, 1) == -1) {
+        return -1;
+    }
+
+    if(tcsetattr(0, TCSANOW, &newt) == -1) {
+        tcsetattr(1, TCSANOW, &oldt);
+    }
+
     return c;
 }
+
+#if defined __cplusplus
+}
+#endif
+
+#endif
+
+#endif
